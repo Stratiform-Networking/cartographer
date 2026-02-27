e31b165bc4fabec1d62dd3e12a04676ecdeab343
DevArtech <iamhaile222@gmail.com>
2026-02-26 23:15:36 -0600

    fix: migration revision length

--- auth-service/migrations/versions/011_create_user_assistant_provider_keys.py
+++ auth-service/migrations/versions/011_create_user_assistant_provider_keys.py
@@ -13,7 +13,7 @@ from alembic import op
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision: str = "011_create_user_assistant_provider_keys"
+revision: str = "011_create_user_assistant_keys"
 down_revision: Union[str, None] = "010_add_avatar_url_to_users"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
